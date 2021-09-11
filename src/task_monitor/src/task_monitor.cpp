c99eaf7893cd312d361f73088fc7062aee36c85c
luislechugaruiz <luislechugaruiz@gmail.com>
2021-09-11 18:30:04 +0800

    Use macro for logging

--- src/task_monitor/src/task_monitor.cpp
+++ src/task_monitor/src/task_monitor.cpp
@@ -1,11 +1,12 @@
 #include "task_monitor.hpp"
 
 #include "pose_3d.hpp"
+#define LOG(level, ...) RCLCPP_##level(this->get_logger(), __VA_ARGS__)
 
 namespace brain {
 
 TaskMonitor::TaskMonitor() : Node("task_monitor") {
-  RCLCPP_INFO(this->get_logger(), "Constructor");
+  LOG(INFO, "constructor");
   initial_pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("initialpose",
                                                                                                   rclcpp::SystemDefaultsQoS());
   this->declare_parameter<double>("initial_x", 0.0);
@@ -14,7 +15,7 @@ TaskMonitor::TaskMonitor() : Node("task_monitor") {
 }
 
 TaskMonitor::~TaskMonitor() {
-  RCLCPP_INFO(this->get_logger(), "Destructor");
+  LOG(WARN, "Destructor");
 }
 
 geometry_msgs::msg::PoseWithCovarianceStamped TaskMonitor::GetInitialPose() {
@@ -28,8 +29,8 @@ geometry_msgs::msg::PoseWithCovarianceStamped TaskMonitor::GetInitialPose() {
   initial_pose_with_covariance_msg.pose = initial_pose.ToMsgWithCovariance();
   initial_pose_with_covariance_msg.header.frame_id = "map";
   initial_pose_with_covariance_msg.header.stamp = rclcpp::Time();
-  RCLCPP_INFO(this->get_logger(), "Initial pose: x: %f" ", y: %f" ", z: %f" ", yaw: %f", 
-              initial_pose.x(), initial_pose.y(), 0.0, initial_pose.yaw());
+  LOG(INFO, "Initial pose: x: %f" ", y: %f" ", z: %f" ", yaw: %f",
+             initial_pose.x(), initial_pose.y(), 0.0, initial_pose.yaw());
   return initial_pose_with_covariance_msg;
 }
 
@@ -37,7 +38,7 @@ void TaskMonitor::Start() {
   // don't do much at this moment, just send the initial pose to nav stack
   initial_pose_ = GetInitialPose();
   initial_pose_publisher_->publish(initial_pose_);
-  RCLCPP_INFO(this->get_logger(), "Initial pose sent to AMCL");
+  LOG(INFO, "Initial pose sent to AMCL");
 }
 
 } // namespace brain
